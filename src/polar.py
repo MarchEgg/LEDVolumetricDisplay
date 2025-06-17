769b7cfdbfb7f1fabd768aa947218c48e0151ee3
Devaj Shourya Bhogireddi <shourya.dsb@gmail.com>
2025-06-16 22:58:19 -0500

    py ver

--- src/polar.py
+++ src/polar.py
@@ -19,3 +19,16 @@ for point in points:
 print("Polar coordinates:")
 for p in polar:
     print(f"r: {p[0]:.2f}, θ: {p[1]:.2f}, z: {p[2]}")
+
+angle = 0
+while True:
+
+    for p in polar:
+        if abs(p[1] - angle) < 0.1:
+            print(f"Point: r: {p[0]}, θ: {angle}, z: {p[2]}")
+        if abs(p[1] - ((angle + m.pi) % (2 * m.pi))) < 0.1:
+            print(f"Point: r: {-1 * p[0]}, θ: {angle}, z: {p[2]}")
+    angle += 0.017
+    
+    if angle > 2 * m.pi:
+        break
